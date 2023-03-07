9767df4f5bc1743f93a1ee0cf67acef7f6b9f952
E.Becheva <34000861+egbecheva@users.noreply.github.com>
2023-03-07 13:54:56 +0200

    initial set up

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const apiKey = process.env.REACT_APP_API_KEY;
+  console.log(apiKey);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
