8ad2028bafd8222b63a9083e0f7d15902d159a64
E.Becheva <34000861+egbecheva@users.noreply.github.com>
2023-03-24 06:43:45 +0200

    WIP:image select functionality

--- src/ImagesGallery.tsx
+++ src/ImagesGallery.tsx
@@ -19,46 +19,59 @@ function ImagesGallery() {
 
   let [imagesData, setImagesData] = useState<ApiResponseData[]>([]);
   let [currentPage, setCurrentPage] = useState<number>(1);
+  let [isSelectedImage, setIsSelectedImage] = useState<boolean>(false);
+  let [imageAltText, setImageAltText] = useState<string>('');
+  // let imageAltText = useRef<string>();
   // let currentPage = useRef(1);
 
-  const COUNT_PHOTOS = 50;
-  const PER_PAGE = 30;
-  const apiKey = process.env.REACT_APP_API_KEY;
-  const countPages = Math.round(COUNT_PHOTOS / PER_PAGE);
+  // const COUNT_PHOTOS = 30;
+  const PER_PAGE: number = 30;
+  const apiKey: string | undefined = process.env.REACT_APP_API_KEY;
+  // const countPages = Math.round(COUNT_PHOTOS / PER_PAGE);
   const fetchImages = async (p: number) => {
     let URL = `https://api.unsplash.com/photos/?client_id=${apiKey}&page=${p}&per_page=${PER_PAGE}`;
     try {
       let response = await fetch(URL);
       let images = await response.json();
+      setImagesData(images);
       return images;
     } catch (e) {
       console.log('Error', e);
     }
   };
 
-  let oldArr: any = [];
-  let currArr: any = [];
+  //in case we need to fetch more pages from API at once
 
-  let baba = async (pg: number) => {
-    for (let i = pg; i <= countPages; i++) {
-      let imgs: any = await fetchImages(i);
-      currArr = [...oldArr, ...imgs];
-      oldArr = currArr;
-    }
-    return currArr;
-  };
+  // let fetchCombinedImages = async (pg: number) => {
+  //   let oldArr: any = [];
+  //   let currArr: any = [];
+  //   for (let i = pg; i <= countPages; i++) {
+  //     let imgs: ApiResponseData[] = await fetchImages(i);
+  //     currArr = [...oldArr, ...imgs];
+  //     oldArr = currArr;
+  //   }
+  //   return currArr;
+  // };
 
   const { data, isSuccess } = useQuery(
     ['images'],
-    () =>
-      baba(currentPage).then((response) => {
-        setImagesData(response);
-        return response;
-      }),
+    () => fetchImages(currentPage),
     {
       keepPreviousData: true,
     }
   );
+
+  const isImageClicked = (alt: string, selected: string | undefined) => {
+    if (isSelectedImage && selected === alt) return true;
+  };
+
+  const isImageBlurred = (alt: string, selected: string | undefined) => {
+    if (isSelectedImage && selected !== alt) {
+      return true;
+    }
+  };
+
+  console.log('rendered');
 
   return (
     <div className='masonry'>
@@ -71,12 +84,28 @@ function ImagesGallery() {
             alt_description: string;
             urls: Urls;
           }) => (
-            <img
-              className='item'
-              key={uuidv4()}
-              alt={alt_description}
-              src={urls?.thumb}
-            />
+            <div>
+              <div className='imageCard'>
+                <img
+                  onClick={() => {
+                    setImageAltText(alt_description);
+                    setIsSelectedImage(true);
+                  }}
+                  className={
+                    isImageClicked(alt_description, imageAltText)
+                      ? 'full-screen'
+                      : isImageBlurred(alt_description, imageAltText)
+                      ? 'blurred'
+                      : 'item'
+                  }
+                  key={uuidv4()}
+                  alt={alt_description}
+                  src={imageAltText === alt_description ? urls.raw : urls.thumb}
+                />
+
+                <div>{alt_description}</div>
+              </div>
+            </div>
           )
         )}
     </div>
